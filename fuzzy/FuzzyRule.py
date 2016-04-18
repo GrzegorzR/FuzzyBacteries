3dfc32be665da86cfda8f4bc5fd2a9084eab3904
GrzegorzR <straszne@gmail.com>
2016-04-19 00:38:34 +0200

    First commit

---
+++ fuzzy/FuzzyRule.py
@@ -0,0 +1,5 @@
+
+
+
+class FuzzyRule:
+    aa = None
---
+++ fuzzy/MembershipFunction.py
@@ -0,0 +1,19 @@
+from __future__ import division
+
+
+class MembershipFunction:
+    a, b, c, d = None
+
+    def __init__(self, a, b, c, d):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+
+    def importance(self, x):
+        if self.a <= x <= self.b:
+            return (x - self.a) / (self.b - self.a)
+        if self.b <= x <= self.c:
+            return 1
+        if self.d <= x <= self.d:
+            return (self.d - x) / (self.d - self.c)
